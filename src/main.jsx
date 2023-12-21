6a3645c1f8dbc189a8823882ee4de2df8a9bbd82
AK-Rasel <raselak101010@gmail.com>
2023-12-22 02:53:56 +0600

    dashbord desgen panding

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,23 @@ import { RouterProvider } from 'react-router-dom'
 import createrouter from './Router/createrouter.jsx'
 import AuthProvider from './Auth/AuthProvider.jsx'
 
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider>
     <RouterProvider router={createrouter} />
     <Toaster />
     </AuthProvider>
+    </QueryClientProvider>
     
   </React.StrictMode>,
 )
